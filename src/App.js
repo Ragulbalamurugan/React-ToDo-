7baa5358ae97998fdf42a101a2096a93c73733b5
Ragulbalamurugan <ragulbrs70871@gmail.com>
2022-06-22 05:19:00 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const ReactToDO = ()=>{
+    const[Task, updateTask]= useState([
+        {
+            text:"Wake up",
+            isCompleted:false
+        },
+        {
+            text:"Fresh up",
+            isCompleted:false
+        },
+        {
+            text:"Boost up",
+            isCompleted:false
+        }
+    ])
+
+    return(
+        <div className='React'>
+            {Task.map((Task,index)=>(
+                <div className='newreact'>
+                    {Task.text}
+                </div>
+            ))}
+        </div>
+    )
 }
+    
 
-export default App;
+export default "ReactToDo";
--- src/index.js
+++ src/index.js
@@ -1,17 +1,81 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+// React ToDo List App
+
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const AddTask = ({addTask})=>{
+    const[value, updateValue]= useState("");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    const handleSubmit = e=>{
+        e.preventDefault();
+        if(value !=="")
+        addTask(value)
+        updateValue("");
+    }
+    
+
+    return(
+        <form  onSubmit={handleSubmit}>
+            <input
+            type="text"
+            value = {value}
+            placeholder="Enter the Task"
+            onChange={e=>updateValue(e.target.value)}
+            />
+            <button type="submit"><i class="material-icons">add</i></button>
+        </form>
+    )
+}
+
+
+ const ToDoList = ()=>{
+
+    const addTask = text=> updateTask([...Task, {text}]);
+    const[Task, updateTask]= useState([
+        {
+            text:"Wake up",
+            isCompleted:false
+        },
+        {
+            text:"Fresh up",
+            isCompleted:false
+        },
+        {
+            text:"Boost up",
+            isCompleted:false
+        },
+    ])
+
+    const removeTask = index =>{
+        const newTask = [...Task]
+        newTask.splice(index, 1)
+        updateTask(newTask);
+    }
+    const toggleTask = index =>{
+        const newTask = [...Task]
+        if(newTask[index].isCompleted)
+        {
+            newTask[index].isCompleted= false
+        }
+        else{
+            newTask[index].isCompleted = true
+        }
+        updateTask(newTask);
+    }
+
+    return(
+        <div className="React-to-do">
+            {Task.map((Task, index)=>(
+                <div className="newreact">
+                    <span onClick={()=>toggleTask(index)} className={Task.isCompleted? "completed":"incompleted"}>
+                    {Task.text}
+                    </span>
+                    <button onClick={()=> removeTask(index)}><i class="material-icons">delete</i> </button>
+                </div>
+                 ))}
+                 <AddTask addTask={addTask}/>
+        </div>
+    )
+ }
+ ReactDOM.render(<ToDoList/>,document.getElementById('root'));
