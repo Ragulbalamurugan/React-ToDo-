879b59aea796e5921fecd8364114fddb4cc70f0d
Ragulbalamurugan <ragulbrs70871@gmail.com>
2022-06-22 05:37:12 +0530

    new commit

--- src/index.js
+++ src/index.js
@@ -1,81 +1,12 @@
 // React ToDo List App
 
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import './index.css';
+import ReactToDo from "./App.js";
 
-const AddTask = ({addTask})=>{
-    const[value, updateValue]= useState("");
-
-    const handleSubmit = e=>{
-        e.preventDefault();
-        if(value !=="")
-        addTask(value)
-        updateValue("");
-    }
-    
-
-    return(
-        <form  onSubmit={handleSubmit}>
-            <input
-            type="text"
-            value = {value}
-            placeholder="Enter the Task"
-            onChange={e=>updateValue(e.target.value)}
-            />
-            <button type="submit"><i class="material-icons">add</i></button>
-        </form>
-    )
-}
+ReactDOM.render(<ReactToDo/>, document.getElementById('root'));
 
 
- const ToDoList = ()=>{
 
-    const addTask = text=> updateTask([...Task, {text}]);
-    const[Task, updateTask]= useState([
-        {
-            text:"Wake up",
-            isCompleted:false
-        },
-        {
-            text:"Fresh up",
-            isCompleted:false
-        },
-        {
-            text:"Boost up",
-            isCompleted:false
-        },
-    ])
-
-    const removeTask = index =>{
-        const newTask = [...Task]
-        newTask.splice(index, 1)
-        updateTask(newTask);
-    }
-    const toggleTask = index =>{
-        const newTask = [...Task]
-        if(newTask[index].isCompleted)
-        {
-            newTask[index].isCompleted= false
-        }
-        else{
-            newTask[index].isCompleted = true
-        }
-        updateTask(newTask);
-    }
-
-    return(
-        <div className="React-to-do">
-            {Task.map((Task, index)=>(
-                <div className="newreact">
-                    <span onClick={()=>toggleTask(index)} className={Task.isCompleted? "completed":"incompleted"}>
-                    {Task.text}
-                    </span>
-                    <button onClick={()=> removeTask(index)}><i class="material-icons">delete</i> </button>
-                </div>
-                 ))}
-                 <AddTask addTask={addTask}/>
-        </div>
-    )
- }
- ReactDOM.render(<ToDoList/>,document.getElementById('root'));
+ 
